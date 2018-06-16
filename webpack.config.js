d81072dcc3ab233df4e25b73642842faaf851dc9
torrichel <torrichel@gmail.com>
2018-06-17 01:39:30 +0400

    Configuring build folder

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     },
     output: {
         filename: 'transformed.js',
-        path: __dirname + '/build',
+        path: __dirname + '/botskulptor',
         publicPath: '/'
     },
     plugins: [
