d4fc12547543adbbde109828ec36557f3cbb76e5
torrichel <torrichel@gmail.com>
2018-06-25 03:17:44 +0400

    Changed builds directory to easily deploy on server

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     },
     output: {
         filename: 'transformed.js',
-        path: __dirname + '/botskulptor',
+        path: __dirname + '/dist',
         publicPath: '/'
     },
     plugins: [
