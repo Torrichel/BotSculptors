94033861b940c50aea3897ee306d44e59bf455d5
torrichel <torrichel@gmail.com>
2018-10-01 05:09:20 +0400

    Added "our services".

--- app/components/Pages/Home.js
+++ app/components/Pages/Home.js
@@ -20,6 +20,7 @@ export class Home extends React.Component {
 
             <div>
 
+                {/* Slider */}
                 <div className="slider" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Slide1.png)` } }>
 
                     <div className="info">
@@ -28,6 +29,48 @@ export class Home extends React.Component {
                         <div className="call_to_action">Наши Услуги</div>
                         <div className="pagination"></div>
                     </div>
+
+                </div>
+
+                {/* Our Services */}
+                <div className="our_services">
+
+                    <div className="title">
+                        <span className="back_text">НАШИ УСЛУГИ</span>
+                        <span className="front_text">НАШИ УСЛУГИ</span>
+                    </div>
+
+                    <div className="services">
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }>
+                            <div className="title">Веб <br/><b>Дизайн</b></div>
+                        </div>
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebsiteDevelopment.png)` } }>
+                            <div className="title">Разработка <br/><b>Сайтов</b></div>
+                        </div>
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/BotDevelopment.png)` } }>
+                            <div className="title">Разработка <br/><b>Ботов</b></div>
+                        </div>
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/ChromeExtensions.png)` } }>
+                            <div className="title">Chrome <br/><b>Extensions</b></div>
+                        </div>
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/Software.png)` } }>
+                            <div className="title">Программное <br/><b>Обеспечение</b></div>
+                        </div>
+
+                        <div className="service" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/Maintenance.png)` } }>
+                            <div className="title">Техническое <br/><b>Обслуживание</b></div>
+                        </div>
+
+
+
+
+                    </div>
+
 
                 </div>
 
