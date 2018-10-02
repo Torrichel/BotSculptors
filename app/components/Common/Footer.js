cb47203085cb2ebc3257b4e884ba44f3fb5e9e45
torrichel <torrichel@gmail.com>
2018-10-02 23:34:25 +0400

    Added all components to a home page. Working on footer.

--- app/components/Common/Footer.js
+++ app/components/Common/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
-
+import logo from '../../static/images/logo.png'
 
 
 export class Footer extends React.Component{
@@ -14,15 +14,56 @@ export class Footer extends React.Component{
     render(){
 
 
-
         return (
 
             <footer>
-                <div className="footerIn">
-                    Copyright 2018
-                </div>
-            </footer>
 
+
+                <div className="footer_first_line">
+
+                    <div className="logo_section">
+                        <div className="sub_section_1">
+                            <NavLink className="logo" to="/">
+                                <img src={logo} alt="Bot Sculptors" />
+                            </NavLink>
+                        </div>
+                        <div className="sub_section_2">
+                            <div className="title"><span>Bot </span><b>Sculptors</b></div>
+                            <div className="slogan">Bot Development and Design</div>
+                        </div>
+                    </div>
+
+                    <nav className="navBlock">
+
+                        {/*<div className="nav_menu_top">*/}
+                            {/*<Link to="/how-it-works">Как это работает?</Link>*/}
+                            {/*<Link to="/team">Услуги</Link>*/}
+                            {/*<Link to="/portfolio">Блог</Link>*/}
+                            {/*<Link to="/about">О нас</Link>*/}
+                            {/*<Link to="/about">Портфолио</Link>*/}
+                            {/*<Link to="/about">Обзоры</Link>*/}
+                            {/*<Link to="/about">Контакты</Link>*/}
+                        {/*</div>*/}
+
+                        {/*<div className="nav_menu_bottom">*/}
+
+                            {/*<div className="fill_brief">Заполнить бриф</div>*/}
+                            {/*<div className="get_quote">Расчет стоимости</div>*/}
+
+                        {/*</div>*/}
+
+                    </nav>
+
+                </div>
+
+                <div className="footer_second_line">
+
+
+
+                </div>
+
+
+            </footer>
 
         );
     }
--- app/components/Pages/Home.js
+++ app/components/Pages/Home.js
@@ -74,6 +74,179 @@ export class Home extends React.Component {
 
                 </div>
 
+                {/* Technologies */}
+                <div className="techs">
+
+                    <div className="title">
+                        <span className="back_text">ТЕХНОЛОГИИ</span>
+                        <span className="front_text">ТЕХНОЛОГИИ</span>
+                    </div>
+
+                    <div className="slogan">
+                        Мы используем только самые последние технологии современного рынка
+                    </div>
+
+                    <div className="technologies">
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="technology" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+
+
+                    </div>
+
+
+                </div>
+
+                {/* Our Portfolio */}
+                <div className="our_portfolio">
+
+                    <div className="title">
+                        <span className="back_text">НАШЕ ПОРТФОЛИО</span>
+                        <span className="front_text">НАШЕ ПОРТФОЛИО</span>
+                    </div>
+
+                    <div className="menu">
+                        <div className="item">Веб-Дизайн</div>
+                        <div className="item">Разработка Сайтов</div>
+                        <div className="item">Разработка Ботов</div>
+                        <div className="item">Chrome Extensions</div>
+                        <div className="item">Программное Обеспечение</div>
+                        <div className="item">Техническое Обслуживание</div>
+                    </div>
+
+                    <div className="projects">
+
+                        <div className="project" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }>
+
+                            <div className="category">РАЗРАБОТКА САЙТОВ</div>
+                            <div className="title"><b>Разработка портала</b><br/>в сфере кансалтинга</div>
+
+                        </div>
+
+                        <div className="project" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }>
+
+                            <div className="category">РАЗРАБОТКА САЙТОВ</div>
+                            <div className="title"><b>Разработка портала</b><br/>в сфере кансалтинга</div>
+
+                        </div>
+
+                        <div className="project" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }>
+
+                            <div className="category">РАЗРАБОТКА САЙТОВ</div>
+                            <div className="title"><b>Разработка портала</b><br/>в сфере кансалтинга</div>
+
+                        </div>
+
+
+                        <div className="project" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }>
+
+                            <div className="category">РАЗРАБОТКА САЙТОВ</div>
+                            <div className="title"><b>Разработка портала</b><br/>в сфере кансалтинга</div>
+
+                        </div>
+
+
+
+                        <div className="more_projects">Больше проектов</div>
+
+
+                    </div>
+
+
+                </div>
+
+                {/* We're trusted by */}
+                <div className="we_are_trusted_by">
+
+                    <div className="title">
+                        <span className="back_text">НАМ ДОВЕРЯЮТ</span>
+                        <span className="front_text">НАМ ДОВЕРЯЮТ</span>
+                    </div>
+
+
+                    <div className="companies">
+
+                        <div className="company" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="company" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="company" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="company" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+
+                    </div>
+
+
+                </div>
+
+                {/* We're featured by */}
+                <div className="we_are_featured_by">
+
+                    <div className="title">
+                        <span className="back_text">НАМ ДОВЕРЯЮТ</span>
+                        <span className="front_text">НАМ ДОВЕРЯЮТ</span>
+                    </div>
+
+
+                    <div className="persons">
+
+                        <div className="person" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+                        <div className="person" style={ { backgroundImage: `url(https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-571535901675/Our+Services/WebDesign.png)` } }></div>
+
+
+                    </div>
+
+
+                </div>
+
+                {/* Subscribe */}
+                <div className="subscription">
+
+                    <div className="title">
+                        Появились вопросы? <br/>
+                        С удовольствием ответим на них!
+                    </div>
+                    <div className="form">
+                        <form className="form" action="" method="POST">
+                            <input type="text" placeholder="Ваш E-mail"/>
+                            <button>Свяжитесь со мной</button>
+                        </form>
+                    </div>
+
+                </div>
+
             </div>
 
 
