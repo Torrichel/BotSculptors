170cf22bc16a1e492e68c7d028f7808114054f92
torrichel <torrichel@gmail.com>
2019-03-05 06:57:09 +0400

    Midified about us page.

--- app/components/Pages/About/About.js
+++ app/components/Pages/About/About.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { Emoji } from 'emoji-mart'
+
 import Popup from '../Popup/Popup'
 
 export class About extends Component {
@@ -101,9 +103,9 @@ export class About extends Component {
 
                     <div className="info-about">
                       <h3>Bot<span> Sculptors.</span></h3>
-                      <p>Мы - единый механизм,<br />
-                        модулирующий и воплощающий <br />
-                        Ваше будущее.
+                      <p>Our goals are creating, <br />
+                        deploying and maintaining<br />
+                        customized solutions for your business.
                       </p>
                     </div>
 
@@ -117,12 +119,20 @@ export class About extends Component {
                   <span className="front_text">ABOUT US</span>
               </div>
               <div className="description">
-                Идейные соображения высшего порядка, а также рамки и место обучения кадров <br/>
-                играет важную роль в формировании направлений прогрессивного развития.<br/>
-                Разнообразный и богатый опыт начало повседневной работы по формированию <br/>
-                позиции способствует подготовки и реализации систем массового участия.
+
+                  <p><b>BotSculptors</b> was born in spring 2018. We are the team of professional Software Developers (SD), Designers and UX specialists with huge experience in programming. Some of us worked in social networks with <b><i>Audience > 1 Million Active Users</i></b>, startups specialized on ChatBot development and CryptoCurrency projects. For more details about our projects, please, visit <a href='/#portfolio'>portfolio</a> section! <Emoji emoji={{ id: 'sunglasses', skin: 3 }} size={16} /></p>
+
+                  <p>Our team combined that various set of skills all together. We really believe that synergy of Crypto Currencies and automated systems can change the world and make it the better place to live.</p>
+
+                  <p>We are digital nomads and prefer to work remotely from different points of our amazing planet. Mobility allows us to cover a lot of USA and Eastern Europe time zones and makes communication with us much more comfortable. We’re the international team and speak fluently English, Russian, Chinese, German, French, Ukrainian and some other exotic local languages <Emoji emoji={{ id: 'smiley', skin: 3 }} size={16} /> </p>
+
+                  <p>We’re focusing mainly on providing our clients with custom Bots or Cryptocurrency solutions, but we are always happy to help you with other programming challenges! Just reach us by clicking on Live Chat icon below. We're available 24/7.</p>
+
+
+
               </div>
-              <div className="call_to_action"><a href="#" className="btn-gradient">Читать полностью</a> </div>
+
+              {/*<div className="call_to_action"><a href="#" className="btn-gradient">Читать полностью</a> </div>*/}
             </div>
 
             <div className="dream-team">
