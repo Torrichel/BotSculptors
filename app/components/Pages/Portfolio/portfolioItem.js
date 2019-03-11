a88ab771f46bedf245cf8c7f96ff4a986e6615c7
Surik266 <surik.pozoyan@gmail.com>
2019-03-11 04:03:17 +0400

    Added in portfolio item page(Gallary,authors,styles for skills)

--- app/components/Pages/Portfolio/portfolioItem.js
+++ app/components/Pages/Portfolio/portfolioItem.js
@@ -6,50 +6,7 @@ import Lightbox from 'react-images';
 import Skills from './Skills';
 import { Redirect} from 'react-router-dom'
 
-const photos = [
-  {
-    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
-    width: 1,
-    height:1,
-    className: 'portfolioItem-gallery-item'
-  }, {
-    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
-    width: 1,
-    height: 1,
-    className: 'portfolioItem-gallery-item'
-  }
 
-];
 
 export class PortfolioItem extends React.Component {
   constructor(props) {
@@ -60,7 +17,9 @@ export class PortfolioItem extends React.Component {
       currentImage: 0,
       url: props.match.params.projectID,
       project: {},
-      pageLoadError: false
+      pageLoadError: false,
+      authors: [],
+      gallery: []
     };
     this.closeLightbox = this.closeLightbox.bind(this);
     this.openLightbox = this.openLightbox.bind(this);
@@ -96,26 +55,43 @@ export class PortfolioItem extends React.Component {
       this.setState({
         project: Object.keys(nextProps.project).length
           ? nextProps.project.project
-          : this.state.project.project
+          : this.state.project.project,
+        authors: Object.keys(nextProps.project).length
+          ? nextProps.project.authors
+          : this.state.project.authors,
+        images: Object.keys(nextProps.project).length
+          ? nextProps.project.project.gallery.images
+          : this.state.project.project.gallery.images
       });
     }
   // Fetch project once component did mount
   componentWillMount() {
 
       this.props.dispatch(getProject(this.state.url))
-      .then((res) => {
-        console.log("result",res)
-      })
+      .then()
       .catch((err) => {
-        this.setState({
-          pageLoadError: true
-        })
+        // this.setState({
+        //   pageLoadError: true
+        // })
       });
 
       scroll(0,0);
     }
 
   render() {
+
+    let photosData = [];
+    if (this.state.images != [] && this.state.images != undefined) {
+      let photos = this.state.images.map((item) => {
+          let itemData = {
+              src: item,
+              width: 1,
+              height: 1,
+              className: 'portfolioItem-gallery-item'
+            }
+          photosData.push(itemData);
+      })
+    }
 
 
 if (!this.state.pageLoadError) {
@@ -132,7 +108,7 @@ if (!this.state.pageLoadError) {
         <p className="projectNameDescription"   dangerouslySetInnerHTML={{ __html: this.state.project.text }}   >
 
         </p>
-        <button type="button" name="button">Read more</button>
+        {/*<button type="button" name="button">Read more</button>*/}
 
       </div>
       <div className="usedTechnology">
@@ -144,34 +120,17 @@ if (!this.state.pageLoadError) {
       <div className="workedOnProject">
         <span>worked on the project:</span>
         <div className="Developers">
-          <div className="developerForThisProject">
-            <img src="https://s3.amazonaws.com/botsculptors/website/person.png"></img>
-            <div className="DeveloperName">
-              <span className="DeveloperName">Семён Семёныч</span>
-              <span className="DeveloperRoleInProject">Front-end developer</span>
+          {  this.state.authors.map((author) =>(
+                <div className="developerForThisProject" key={author._id}>
+                    <img src={author.photo}></img>
+                    <div className="DeveloperName">
+                      <span className="DeveloperName">{author.firstName} {author.lastName}</span>
+                      <span className="DeveloperRoleInProject">Front-end developer</span>
 
-            </div>
-            <button type="button" name="developerPage">&#8250;</button>
-          </div>
-          <div className="developerForThisProject">
-            <img src="https://s3.amazonaws.com/botsculptors/website/person1.png"></img>
-            <div className="DeveloperName">
-              <span className="DeveloperName">Семён Семёныч</span>
-              <span className="DeveloperRoleInProject">Front-end developer</span>
-
-            </div>
-            <button type="button" name="developerPage">&#8250;</button>
-          </div>
-          <div className="developerForThisProject">
-            <img src="https://s3.amazonaws.com/botsculptors/website/person.png"></img>
-            <div className="DeveloperName">
-              <span className="DeveloperName">Семён Семёныч</span>
-              <span className="DeveloperRoleInProject">Front-end developer</span>
-
-            </div>
-            <button type="button" name="developerPage">&#8250;</button>
-          </div>
-
+                    </div>
+                    <button type="button" name="developerPage">&#8250;</button>
+                  </div>
+              ))}
         </div>
       </div>
       <div className="PortfolioImgGallery">
@@ -181,8 +140,8 @@ if (!this.state.pageLoadError) {
       </div>
 
     </div>
-    <Gallery photos={photos} onClick={this.openLightbox}/>
-    <Lightbox images={photos} onClose={this.closeLightbox} onClickPrev={this.gotoPrevious} onClickNext={this.gotoNext} currentImage={this.state.currentImage} isOpen={this.state.lightboxIsOpen}/>
+    <Gallery photos={photosData != [] ? photosData : {}} onClick={this.openLightbox}/>
+    <Lightbox images={photosData != [] ? photosData : {}} onClose={this.closeLightbox} onClickPrev={this.gotoPrevious} onClickNext={this.gotoNext} currentImage={this.state.currentImage} isOpen={this.state.lightboxIsOpen}/>
   </div>)
 }
   return <Redirect to="/" />
