f11f033b74a5ff5c7655bd1512b5bfb2ffa10817
torrichel <torrichel@gmail.com>
2019-04-11 18:11:41 +0400

    Removed some modules, added responsive styles to about page.

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 import { userActions } from '../../../actions';
 
-import { Emoji } from 'emoji-mart';
 
 import Popup from '../Popup/Popup';
 
@@ -97,7 +96,6 @@ export class About extends Component {
                                 <div className="info">
 
                                     <div className="info-about">
-                                        <h3>Bot<span> Sculptors.</span></h3>
                                         <p>Our goals are creating, <br />
                                             deploying and maintaining<br />
                                             customized solutions for your business.
@@ -115,11 +113,11 @@ export class About extends Component {
                                 </div>
                                 <div className="description">
 
-                                    <p><b>BotSculptors</b> was born in spring 2018. We are the team of professional Software Developers (SD), Designers and UX specialists with huge experience in programming. Some of us worked in social networks with <b><i>Audience > 1 Million Active Users</i></b>, startups specialized on ChatBot development and CryptoCurrency projects. For more details about our projects, please, visit <a href='/#portfolio'>portfolio</a> section! <Emoji emoji={{ id: 'sunglasses', skin: 3 }} size={16} /></p>
+                                    <p><b>BotSculptors</b> was born in spring 2018. We are the team of professional Software Developers (SD), Designers and UX specialists with huge experience in programming. Some of us worked in social networks with <b><i>Audience > 1 Million Active Users</i></b>, startups specialized on ChatBot development and CryptoCurrency projects. For more details about our projects, please, visit <a href='/#portfolio'>portfolio</a> section!</p>
 
                                     <p>Our team combined that various set of skills all together. We really believe that synergy of Crypto Currencies and automated systems can change the world and make it the better place to live.</p>
 
-                                    <p>We are digital nomads and prefer to work remotely from different points of our amazing planet. Mobility allows us to cover a lot of USA and Eastern Europe time zones and makes communication with us much more comfortable. We’re the international team and speak fluently English, Russian, Chinese, German, French, Ukrainian and some other exotic local languages <Emoji emoji={{ id: 'smiley', skin: 3 }} size={16} /> </p>
+                                    <p>We are digital nomads and prefer to work remotely from different points of our amazing planet. Mobility allows us to cover a lot of USA and Eastern Europe time zones and makes communication with us much more comfortable. We’re the international team and speak fluently English, Russian, Chinese, German, French, Ukrainian and some other exotic local languages. </p>
 
                                     <p>We’re focusing mainly on providing our clients with custom Bots or Cryptocurrency solutions, but we are always happy to help you with other programming challenges! Just reach us by clicking on Live Chat icon below. We're available 24/7.</p>
 
